a019f1c020f28da2344e6dd5335117f60a12723a
Alejandro CR <contacto@alejandrocr.co>
2026-02-03 20:11:14 -0500

    feat: add 1-column layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './layouts/app'
+import { App } from './layouts/1-column'
 
 import './tailwind.css'
 
