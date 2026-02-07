25caad608f26f834d495f4ab531ae558cb7c7ca8
Alejandro CR <contacto@alejandrocr.co>
2026-02-07 12:34:40 -0500

    add with prop to 1 column layout

--- src/layouts/1-column.tsx
+++ src/layouts/1-column.tsx
@@ -3,16 +3,56 @@ import { AppShell } from './shell'
 type Props = {
   columnTitle?: string
   columnNode?: React.ReactNode
+  columnWidth?:
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | '2xl'
+    | '3xl'
+    | '4xl'
+    | '5xl'
+    | '6xl'
+    | '7xl'
+    | 'full'
   navigation: { name: string; href: string; current?: boolean }[]
   user: { name: string; email: string; imageUrl: string }
   userNavigation: { name: string; href: string }[]
 }
 
+const widthClasses = {
+  sm: 'max-w-sm',
+  md: 'max-w-md',
+  lg: 'max-w-lg',
+  xl: 'max-w-xl',
+  '2xl': 'max-w-2xl',
+  '3xl': 'max-w-3xl',
+  '4xl': 'max-w-4xl',
+  '5xl': 'max-w-5xl',
+  '6xl': 'max-w-6xl',
+  '7xl': 'max-w-7xl',
+  full: 'max-w-full'
+}
+
 export function Layout(props: Props) {
-  const { columnTitle, columnNode, navigation, user, userNavigation } = props
+  const {
+    columnTitle,
+    columnNode,
+    columnWidth = '7xl',
+    navigation,
+    user,
+    userNavigation
+  } = props
+
   return (
-    <AppShell navigation={navigation} user={user} userNavigation={userNavigation}>
-      <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+    <AppShell
+      navigation={navigation}
+      user={user}
+      userNavigation={userNavigation}
+    >
+      <div
+        className={`mx-auto ${widthClasses[columnWidth]} px-4 pb-12 sm:px-6 lg:px-8`}
+      >
         <section aria-labelledby="section-1-title">
           <h2 id="section-1-title" className="sr-only">
             {columnTitle}
